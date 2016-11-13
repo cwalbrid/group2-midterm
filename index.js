4891c8f1c892bfc70686d36d355dfd58f85051df
Colin Walbridge <cwalbrid@umich.edu>
2016-11-13 18:01:04 -0500

    Created an array for selectedSeats, wrote script for toggling form opacity, wrote loop for capturing selectedSeats values

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 $(document).ready(function (){
 
+var selectedSeats = [];
+
 $('.open').on('click', function(){
 	$(this).toggleClass('open selected');
 	
 	
 	var seatNumber = $(this).attr('id'); 
 	var seatClass = $(this).attr('class');
+	var formOpacity = $('.form').css('opacity');
 	
 	if(seatClass === 'seats selected'){
-		console.log(seatClass);
+		// console.log(seatClass);
+		selectedSeats.push(seatNumber);
+
+		for(var i = 0; i < selectedSeats.length; i++){
+			fetchedSeat = selectedSeats[i]; 
+			console.log(fetchedSeat);
+		 }
 	}
 
 	//toggle visibility for form with conditional statement that accepts 'open' classes
-	/*
-	var formVis = $('.form').css('visibility');
-
-	if(formVis === 'hidden'){
-		$('form').css('visibility', 'visible');
+	
+	if(formOpacity === '0'){
+		$('.form').css('opacity', '1');
 	}
-	*/
+	
 
 	//'Seat(s) Selected' field is populated with clicked seat's ID value
 	/*
-		s
+		
 	*/
 
 });
