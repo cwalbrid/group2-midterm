22d201fc342aa3a6fffaff601786c1fc751db962
Colin Walbridge <cwalbrid@umich.edu>
2016-11-13 16:43:22 -0500

    added seatNumber and seatClass variables; wrote conditional statement to toggle form visibility

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 $(document).ready(function (){
-$('.seats').on('click', function(){
-	alert('hello!!');
+
+$('.open').on('click', function(){
+	$(this).toggleClass('open selected');
+	
+	
+	var seatNumber = $(this).attr('id'); 
+	var seatClass = $(this).attr('class');
+	
+	if(seatClass === 'seats selected'){
+		console.log(seatClass);
+	}
+
+	//toggle visibility for form with conditional statement that accepts 'open' classes
+	/*
+	var formVis = $('.form').css('visibility');
+
+	if(formVis === 'hidden'){
+		$('form').css('visibility', 'visible');
+	}
+	*/
+
+	//'Seat(s) Selected' field is populated with clicked seat's ID value
+	/*
+		
+	*/
+
 });
 
+
+	
+
+//document.body wrapper
 });
