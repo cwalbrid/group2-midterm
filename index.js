5783f48dcf23d36e5830b151cbdc071f7092075f
Colin Walbridge <cwalbrid@umich.edu>
2016-11-14 15:12:34 -0500

    added event handler for reserve seats button that changes opacity of form back to 0 and caches user info in as an object into an array

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ $(document).ready(function (){
 
 // An array that stores the seat numbers that a user has selected
 var selectedSeats = [];
+// An array that stores user information on form submit
+var userInfo = [];
+
+// An event handler for when the button w/id 'reservebutton' is clicked
+	$('#reservebutton').on('click', function(){
+		var name = $('#name').val();
+		var reservedSeats = $('#seatsToBeReserved').val();
+		var userObject = {
+			name: name,
+			seat: reservedSeats
+		};
+		userInfo.push(userObject);
+		console.log(userInfo);
+
+		$('#name').val('');
+		$('#seatsToBeReserved').val('');
+
+		$('.selected').removeClass('selected').removeClass('seats').addClass('reserved');
+});
 
 // A handler for when a user clicks on a seat with a class of Open or Selected
 $('.seats').on('click', function(){
 	
-	var seatNumber = $(this).attr('id'); 
+	var seatNumber = " " + $(this).attr('id').toUpperCase(); 
 	var seatClass = $(this).attr('class');
-	var formOpacity = $('.form').css('opacity');
+	var formOpacity = $('#form').css('opacity');
 	
 	// Push the seat number to our selectedSeats array
 	if(seatClass === 'seats open'){
@@ -18,7 +37,7 @@ $('.seats').on('click', function(){
 
 	// Sets form opacity to 1 if the current opacity is set to 0. This makes the form visible.
 		if(formOpacity === '0'){
-			$('.form').css('opacity', '1');
+			$('#form').css('opacity', '1');
 	};	 
 	
 	//Remove seats from the selectedSeats array if they have a class of selected and are clicked again.
@@ -28,22 +47,19 @@ $('.seats').on('click', function(){
 			return e !== seatNumber;
 		});
 
-		alert(selectedSeats)
+		
 	};
+
+	//'Seat(s) Selected' field is populated with clicked seat's ID value
+	$('#seatsToBeReserved').val(selectedSeats);
 
 	//Toggle seat class between 'open' and 'selected'
 	$(this).toggleClass('open selected');
 	
 
-
-	
-
-	//'Seat(s) Selected' field is populated with clicked seat's ID value
-	/*
-		
-	*/
-
 });
+	
+	
 
 
 	
